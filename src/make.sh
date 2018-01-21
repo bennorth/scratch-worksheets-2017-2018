6655e5bc5fc7f69313348cfca11730575bd6a0cc
Ben North <ben@redfrontdoor.org>
2018-01-21 17:54:01 +0000

    Add local CSS

--- src/make.sh
+++ src/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pandoc index.md -c github-markdown.css \
+pandoc index.md -c github-markdown.css -c scratch-sheets.css \
        -B body-head.html -A body-foot.html -o ../docs/index.html
